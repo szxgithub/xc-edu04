fadf201bd7b220688c2a8f9fb8185b01e033de31
miaoruntu <1759102882@qq.com>
2018-09-30 20:52:40 +0800

    修改...

--- xc-govern-center/src/main/java/com/xuecheng/govern/center/GovernCenterApplication.java
+++ xc-govern-center/src/main/java/com/xuecheng/govern/center/GovernCenterApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  * @author Administrator
  * @version 1.0
  **/
-@EnableEurekaServer //标识此工程是一个EurekaServer
+@EnableEurekaServer // 标识此工程是一个EurekaServer
 @SpringBootApplication
 public class GovernCenterApplication {
     public static void main(String[] args) {
